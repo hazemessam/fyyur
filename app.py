759f67138d9b164c02e263705ac1b806c1d10588
Hazem <hazemkwita123@gmail.com>
2020-10-14 09:42:56 +0200

    create models.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -23,58 +23,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-  __tablename__ = 'venues'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  genres = db.Column(db.ARRAY(db.String()))
-  address = db.Column(db.String(120))
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
-  website = db.Column(db.String(120))
-  seeking_talent = db.Column(db.Boolean)
-  seeking_description = db.Column(db.String(500))
-  
-  artists = db.relationship('Artist', secondary='shows', backref=db.backref('venues', lazy=True))
-
-  def __repr__(self):
-    return f'<Venue {self.id} {self.name}>'
-
-
-class Artist(db.Model):
-  __tablename__ = 'artists'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  genres = db.Column(db.ARRAY(db.String))
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
-  website = db.Column(db.String(120))
-  seeking_venue = db.Column(db.Boolean)
-  seeking_description = db.Column(db.String(500))
-
-  def __repr__(self):
-    return f'<Artist {self.id} {self.name}>'
-
-
-class Show(db.Model):
-  __tablename__ =  'shows'
-  id = db.Column(db.Integer, primary_key=True)
-  venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
-  start_time = db.Column(db.DateTime, nullable=False)
-
-  def __repr__(self):
-    return f'<Show {self.venue_id} {self.artist_id} {self.start_time}>'
+from models import *
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -103,6 +52,13 @@ def index():
 
 @app.route('/venues')
 def venues():
+  """
+  
+  Keyword arguments:
+  argument -- description
+  Return: return_description
+  """
+  
   areas_query = Venue.query.with_entities(Venue.city, Venue.state).order_by(Venue.city).all()
   areas = set(areas_query)
   data = []
